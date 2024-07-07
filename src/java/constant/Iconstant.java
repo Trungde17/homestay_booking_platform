8205405fb74650874c0d25738d0442f33a010d55
Trung Nguyen <nguyenvietthanhtrunga3@gmail.com>
2024-07-07 21:40:38 +0700

    Bổ sung

--- src/java/constant/Iconstant.java
+++ src/java/constant/Iconstant.java
@@ -2,15 +2,5 @@
 package constant;
 
 public class Iconstant {
-    public static final String GOOGLE_CLIENT_ID = "922603002284-n5btgmn4lpnl1e3kim0ffe4q6ki3c863.apps.googleusercontent.com";
-
-    public static final String GOOGLE_CLIENT_SECRET = "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";
-
-    public static final String GOOGLE_REDIRECT_URI = "http://localhost:8081/healingland/login_servlet";
-
-    public static final String GOOGLE_GRANT_TYPE = "authorization_code";
-
-    public static final String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
-
-    public static final String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    
 }
