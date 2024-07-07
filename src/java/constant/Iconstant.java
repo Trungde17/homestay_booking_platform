861e4ac1f78d387c769a2a7109964f7dc58f0678
Trung Nguyen <nguyenvietthanhtrunga3@gmail.com>
2024-07-07 22:05:24 +0700

    Bổ sung

--- src/java/constant/Iconstant.java
+++ src/java/constant/Iconstant.java
@@ -2,7 +2,9 @@
 package constant;
 
 public class Iconstant {
-    
+    /*public static final String GOOGLE_CLIENT_ID = "922603002284-n5btgmn4lpnl1e3kim0ffe4q6ki3c863.apps.googleusercontent.com";
+
+    public static final String GOOGLE_CLIENT_SECRET = "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";*/
     public static final String GOOGLE_CLIENT_ID = "REDACTED_CLIENT_ID";
     public static final String GOOGLE_CLIENT_SECRET = "REDACTED_CLIENT_SECRET";
 
